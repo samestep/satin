cc2209a3e7fab10e3041bdaf6ce3d88724884e3c
Sam Estep <sam@samestep.com>
2026-09-19 20:04:12 -0400

    Use nixfmt

--- flake.nix
+++ flake.nix
@@ -2,18 +2,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
-    in {
-      packages = forAllSystems (pkgs: rec {
-        default = pkgs.callPackage ./default.nix {
-          version = toString (self.lastModified or 0);
-        };
-        icon = pkgs.runCommand "satin.svg" { nativeBuildInputs = [ pkgs.nodejs ]; } ''
-          node ${./logo/render.mjs} > "$out"
-        '';
-      });
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (
+        system: pkgs: {
+          default = pkgs.callPackage ./default.nix {
+            version = toString (self.lastModified or 0);
+          };
+          icon = pkgs.runCommand "satin.svg" { nativeBuildInputs = [ pkgs.nodejs ]; } ''
+            node ${./logo/render.mjs} > "$out"
+          '';
+        }
+      );
+      formatter = forAllSystems (system: pkgs: pkgs.nixfmt-tree);
+      checks = forAllSystems (
+        system: pkgs: {
+          format = self.formatter.${system}.check self;
+        }
+      );
     };
 }
